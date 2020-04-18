0ae3ffec8df251d75f09596ad09e76a24b7a2dd8
LuisLG007 <LuisLSC@outlook.com>
2020-04-18 01:29:26 -0500

    cambios

--- src/app/pages/plantilla1/plantilla1.page.ts
+++ src/app/pages/plantilla1/plantilla1.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Componente } from '../../interfaces/interfaces';
-import { PuntosR } from '../../interfaces/interfaces';
-import { PeligroE } from '../../interfaces/interfaces';
 import { Observable } from 'rxjs';
 import { DataService } from '../../services/data.service';
 import { ActivatedRoute } from '@angular/router';
@@ -14,23 +12,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Plantilla1Page implements OnInit {
 
-  id2: any;
-  temp = []
+  id2: string;
+  temp : Observable<any>;
   componentes: Componente[] = [];
-
-  PuntosR2 = [];
-
-  PeligroE2 = [];
-
-
   constructor( private menuCtrl: MenuController, private activatedRoute: ActivatedRoute,  private dataService: DataService) { }
 
   ngOnInit() {
-
     this.id2 = this.activatedRoute.snapshot.paramMap.get('id');
     console.log(this.id2);
     this.Comprueba();
-
   }
 
   toggleMenu() {
@@ -40,16 +30,10 @@ export class Plantilla1Page implements OnInit {
   Comprueba() {
     if (this.id2 == 'PuntosR')
     {
-      this.dataService.getPuntosR().subscribe((data: any) => {
-        this.temp = data;       
-      });
-
+      this.temp = this.dataService.getPuntosR();
     } else if (this.id2 == 'PeligroE')
     {
-      this.dataService.getPeligroE().subscribe((data: any) => {
-        this.temp = data;       
-       });
-
+      this.temp = this.dataService.getPeligroE();
     }
-    }//fin del metodo
+  }//fin del metodo
 }
